d8ded919667073c830f6de76380b832870046c5c
Torio Yoshiko <torio.yoshiko@gmail.com>
2018-05-02 12:00:25 +0300

    three

--- js/main.js
+++ js/main.js
@@ -4,20 +4,23 @@
         todoList = JSON.parse(localStorage.getItem('todo'));
     }*/
 
+
     function save () {
         var inputText = document.getElementById('in').value;
         todoList.push(inputText);
         console.log(todoList);
         var i = out();
         console.log(i);
+        document.getElementById("in").value = "";
+        document.getElementById("output").innerHTML = i;
     }
 
     function out() {
         let outTask = '';
         for (let i = 0; i < todoList.length; i++) {
-            outTask += '<div class="output">' + todoList[i] +'</div>';
+            outTask += '<div class="output">' + '<input type="checkbox">' + todoList[i] +'</div>';
         }
-        document.getElementById("output").innerHTML = outTask;
-        document.getElementById("in").value = ""
         return outTask;
     }
+
+   
